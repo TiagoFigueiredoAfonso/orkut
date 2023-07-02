288b2b4a34febd8a56b4f749ff48678707a54cc4
Tiago <67653921+TiagoFigueiredoAfonso@users.noreply.github.com>
2023-07-02 18:05:58 -0400

    projeto orkut

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+let msg = document.querySelector('.message')
+let bd = document.querySelector('body')
+
+
+let tamanho_tela = window.innerWidth;
+
+window.addEventListener('load', ()=>{
+    if(tamanho_tela <= 375){
+      setTimeout(() => {
+            msg.style.opacity = 1;
+            msg.style.display = 'block'
+              
+            setTimeout(()=>{
+              msg.style.opacity = 0;
+              setTimeout(()=>{
+                msg.style.display = 'none';
+              },500)
+        }, 15000)
+       }, 1000);
+    }else {
+      msg.style.display = 'nonne'
+    }
+}) 
+
+let btnclosed = document.querySelector('.btn-message-close')
+
+btnclosed.addEventListener('click', ()=>{
+   msg.style.display = 'none';
+})
